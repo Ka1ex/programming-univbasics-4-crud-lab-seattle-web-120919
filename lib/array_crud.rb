5ec025b2f3fd5cc79bd9f4515d9ac0eda87e158d
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-01 00:48:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  [[], [], [],[]]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << "new end of the array"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.shift("New beginnings")
 end
 
 def remove_element_from_end_of_array(array)
