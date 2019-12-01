8b89ca8b760c939df2d720e384df88178f170b50
Nate Montgomery <kilikalbright@hotmail.com>
2019-12-01 00:55:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.shift("New beginnings")
+  array.shift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[1]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-array[1] = "Newbie"
+array[1] = "totally"
 end
